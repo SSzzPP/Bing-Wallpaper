cb8cccffa93aa841b09045e620b609fc22070532
SSzzPP <zxsunzhangping@126.com>
2023-09-07 16:01:21 +0800

    init

---
+++ src/main/java/com/wdbyte/bing/HttpUtls.java
@@ -0,0 +1,52 @@
+package com.wdbyte.bing;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtls {
+
+    /**
+     * Getting an HTTP connection
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    public static HttpURLConnection getHttpUrlConnection(String url) throws IOException {
+        URL httpUrl = new URL(url);
+        HttpURLConnection httpConnection = (HttpURLConnection)httpUrl.openConnection();
+        httpConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+        return httpConnection;
+    }
+
+    /**
+     * Request a specific URL to return content
+     *
+     * @param url
+     * @return
+     * @throws IOException
+     */
+    public static String getHttpContent(String url) throws IOException {
+        HttpURLConnection httpUrlConnection = getHttpUrlConnection(url);
+        StringBuilder stringBuilder = new StringBuilder();
+        // Getting the input stream
+        try (InputStream input = httpUrlConnection.getInputStream(); BufferedInputStream bis = new BufferedInputStream(
+            input);) {
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            // Read to the end of the file returns -1
+            while ((len = bis.read(buffer)) != -1) {
+                stringBuilder.append(new String(buffer, 0, len));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            httpUrlConnection.disconnect();
+        }
+        return stringBuilder.toString();
+    }
+
+}
